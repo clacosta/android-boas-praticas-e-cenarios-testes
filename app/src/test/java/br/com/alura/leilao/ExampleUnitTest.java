b97b62c0fb7cf9eb79777a07c11d6fa999a272ab
Claudio Costa <claudio@imaginaction.com.br>
2019-10-23 19:45:08 -0300

    feet: 05 - 01 - Conhecendo outras possibilidades de asserts

--- app/src/test/java/br/com/alura/leilao/ExampleUnitTest.java
+++ app/src/test/java/br/com/alura/leilao/ExampleUnitTest.java
@@ -2,6 +2,9 @@ package br.com.alura.leilao;
 
 import org.junit.Test;
 
+import br.com.alura.leilao.model.Usuario;
+
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
 
 /**
@@ -10,8 +13,17 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+        assertNotEquals(3, 2 + 2);
+        assertTrue(true);
+        assertFalse(false);
+        assertNull(null);
+        assertNotNull(new Usuario("Alex"));
+        assertThat(2 + 2,  equalTo(4));
     }
+
+
 }
