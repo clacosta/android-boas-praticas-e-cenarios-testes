be1d8b616c8b41f3b3a7b52c170d1e5af304adee
Claudio Costa <claudio@imaginaction.com.br>
2019-10-23 20:50:41 -0300

    feet: 05 - 03 - Utilizando o assertThat

--- app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
+++ app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -1,8 +1,7 @@
 package br.com.alura.leilao.model;
 
-import org.junit.Rule;
+import org.hamcrest.Matchers;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import java.util.List;
 
@@ -11,8 +10,14 @@ import br.com.alura.leilao.exception.LanceMenorQueOUltimoLanceException;
 import br.com.alura.leilao.exception.LanceSeguidoDoMesmoUsuarioException;
 import br.com.alura.leilao.exception.UsuarioJaDeuCincoLancesException;
 
+import static org.hamcrest.CoreMatchers.both;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.number.IsCloseTo.closeTo;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertThat;
 
 public class LeilaoTest {
 
@@ -23,17 +28,17 @@ public class LeilaoTest {
 
     @Test
     public void deve_DevolveDescricao_QuandoRecebeDescricao_() {
-        //Executar ação esperada
         String descricaoDevolvida = CONSOLE.getDescricao();
-        //Testar resultado esperado
-        assertEquals("Console", descricaoDevolvida);
+//        assertEquals("Console", descricaoDevolvida);
+        assertThat(descricaoDevolvida, is(equalTo("Console")));
     }
 
     @Test
     public void deve_DevolveMaiorLance_QuandoRecebeApenasUmLance() {
         CONSOLE.propoe(new Lance(ALEX, 200.0));
         double maiorLance = CONSOLE.getMaiorLance();
-        assertEquals(200.0, maiorLance, DELTA);
+//        assertEquals(200.0, maiorLance, DELTA);
+        assertThat(maiorLance, closeTo(200.0, DELTA));
     }
 
     @Test
@@ -65,10 +70,21 @@ public class LeilaoTest {
         CONSOLE.propoe(new Lance(FRAN, 300.0));
         CONSOLE.propoe(new Lance(ALEX, 400.0));
         List<Lance> tresMaioredsLances = CONSOLE.tresMaioresLances();
-        assertEquals(3, tresMaioredsLances.size());
-        assertEquals(400.0, tresMaioredsLances.get(0).getValor(), DELTA);
-        assertEquals(300.0, tresMaioredsLances.get(1).getValor(), DELTA);
-        assertEquals(200.0, tresMaioredsLances.get(2).getValor(), DELTA);
+//        assertEquals(3, tresMaioredsLances.size());
+//        assertThat(tresMaioredsLances, hasSize(equalTo(3)));
+////        assertEquals(400.0, tresMaioredsLances.get(0).getValor(), DELTA);
+////        assertThat(tresMaioredsLances, hasItem(new Lance(ALEX, 400.0)));
+////        assertEquals(300.0, tresMaioredsLances.get(1).getValor(), DELTA);
+////        assertEquals(200.0, tresMaioredsLances.get(2).getValor(), DELTA);
+//        assertThat(tresMaioredsLances,
+//                contains(new Lance(ALEX, 400.0),
+//                        new Lance(FRAN, 300.0),
+//                        new Lance(ALEX, 200.0)));
+        assertThat(tresMaioredsLances,
+                both(Matchers.<Lance>hasSize(equalTo(3)))
+                        .and(contains(new Lance(ALEX, 400.0),
+                                new Lance(FRAN, 300.0),
+                                new Lance(ALEX, 200.0))));
     }
 
     @Test
